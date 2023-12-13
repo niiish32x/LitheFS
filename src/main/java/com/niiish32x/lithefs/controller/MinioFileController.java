e3e6b9b802111fa2ca0c3b8c7267fcfb284f5783
Niii-sh <1159417019@qq.com>
2023-12-13 19:53:47 +0800

    feature: 创建Minio文件上传与下载请求实体 优化代码结果

--- src/main/java/com/niiish32x/lithefs/controller/MinioFileController.java
+++ src/main/java/com/niiish32x/lithefs/controller/MinioFileController.java
@@ -1,11 +1,11 @@
 package com.niiish32x.lithefs.controller;
 
+import com.niiish32x.lithefs.dto.req.MinioDownloadReqDTO;
+import com.niiish32x.lithefs.dto.req.MinioUploadReqDTO;
 import com.niiish32x.lithefs.service.MinioFileService;
 import io.minio.errors.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
@@ -14,31 +14,18 @@ import java.security.NoSuchAlgorithmException;
 @RestController
 @RequiredArgsConstructor
 public class MinioFileController {
-    private MinioFileService minioFileService;
+    private final MinioFileService minioFileService;
 
-    /**
-     *
-     * @param bucketName 要进行下载的桶
-     * @param objectName 要下载的文件名
-     * @param downloadPath 本地存储的文件路径
-     */
     @PostMapping("/api/minio/download")
-    public void MinioDownload(@RequestParam("bucketName")String bucketName, @RequestParam("objectName") String objectName, @RequestParam("downloadPath") String downloadPath){
-        minioFileService.downloadFile(bucketName,objectName,downloadPath);
+    public void MinioDownload(@RequestBody MinioDownloadReqDTO requestParam){
+        minioFileService.downloadFile(requestParam.getBucketName(), requestParam.getObjectName(), requestParam.getDownloadPath());
     }
 
 
 
-    /**
-     *
-     * @param bucketName  要存储的桶
-     * @param objectName  要存储的对象
-     * @param uploadFileName    要存储的文件具体路径
-     * @return
-     */
     @PostMapping("/api/minio/upload")
-    public void MinioUpload(@RequestParam("bucketName") String bucketName,@RequestParam("objectName") String objectName, @RequestParam("uploadFileName") String uploadFileName ) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-        minioFileService.uploadFile(bucketName,objectName,uploadFileName);
+    public void MinioUpload(@RequestBody MinioUploadReqDTO requestParam) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        System.out.println("xxx");
+        minioFileService.uploadFile(requestParam.getBucketName(), requestParam.getObjectName(), requestParam.getUploadFileName());
     }
-
 }
