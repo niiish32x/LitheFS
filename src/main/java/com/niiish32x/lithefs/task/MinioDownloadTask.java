2b9f916fdf29a9753d82e8966f117a2b7e679463
Niii-sh <1159417019@qq.com>
2023-12-14 15:06:17 +0800

    feature: 完成minio定时下载功能 定时从minio指定bucket 每分钟下载最新的文件至本地文件夹

---
+++ src/main/java/com/niiish32x/lithefs/task/MinioDownloadTask.java
@@ -0,0 +1,36 @@
+package com.niiish32x.lithefs.task;
+
+import com.niiish32x.lithefs.service.MinioFileService;
+import com.niiish32x.lithefs.tools.MinioInit;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Component
+@RequiredArgsConstructor
+public class MinioDownloadTask {
+    private final MinioFileService minioFileService;
+    private final MinioInit minioInit;
+
+    @Value(value = "${custom.minioListenedBucket}")
+    private String listenedBucket;
+
+
+    /**
+     * 定时任务
+     * 定时从Minio指定的bucket 下载文件 文件至本地指定的文件夹
+     */
+    @SneakyThrows
+    @Scheduled(cron = "* * * * *")  // 每分钟从minio指定的bucket下载文件到本地的文件夹
+    public void download(){
+        System.out.println(listenedBucket);
+        Path path = Paths.get("./tmp/download");
+        minioFileService.downloadAllFileOverwrite(listenedBucket,path.toString());
+    }
+}
