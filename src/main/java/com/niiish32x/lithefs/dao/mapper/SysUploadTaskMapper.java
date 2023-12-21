1f17938a179d7506d37c42a0f37a6d6a53006654
Niii-sh <1159417019@qq.com>
2023-12-21 19:57:40 +0800

    feature:  定时任务上传实体以及mapper

---
+++ src/main/java/com/niiish32x/lithefs/dao/mapper/SysUploadTaskMapper.java
@@ -0,0 +1,12 @@
+package com.niiish32x.lithefs.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.niiish32x.lithefs.dao.entity.SysUploadTask;
+
+/**
+ * 分片上传-分片任务记录(SysUploadTask)表数据库访问层
+
+ */
+public interface SysUploadTaskMapper extends BaseMapper<SysUploadTask> {
+
+}
