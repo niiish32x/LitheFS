a801c477dc3994c878003c69e41f7c220bf769d7
niiish32x <1159417019@qq.com>
2023-12-23 17:19:14 +0800

    refactor: 回调架构 重写分片上传模块 之前写的太乱了

--- src/main/java/com/niiish32x/lithefs/LitheFsApplication.java
+++ src/main/java/com/niiish32x/lithefs/LitheFsApplication.java
@@ -1,10 +1,18 @@
 package com.niiish32x.lithefs;
 
+
+import org.apache.ibatis.annotations.Mapper;
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+//@EnableScheduling
+//@MapperScan("com.niiish32x.lithefs.dao.mapper")
+
+// 这个必须是指定具体的mapper报名才可以顺利 否则就会报错
+@MapperScan("com.niiish32x.lithefs.dao.mapper")
 @SpringBootApplication
 public class LitheFsApplication {
 
